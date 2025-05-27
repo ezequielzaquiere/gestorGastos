c892237cae4cf8c0a8a2452c449471e9967a3faa
Ezequiel Zaquiere <ezequiel.zaquiere@gmail.com>
2025-05-27 19:25:51 +0200

    feat: creando estructura principal

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-red-600">¡Funciona Tailwind!</h1>
+    </div>
+  );
+};
+
+export default App;
+
+
